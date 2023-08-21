8c232fb25c1e4f860e290e9d57001f1b26ca25d8
Ahsan <ahsanaz461@gmail.com>
2023-08-21 11:25:09 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#000',
+        'secondary': '#000',
+        'tertiary': '#000'
+      }
+    },
+  },
+  plugins: [],
+}
+
